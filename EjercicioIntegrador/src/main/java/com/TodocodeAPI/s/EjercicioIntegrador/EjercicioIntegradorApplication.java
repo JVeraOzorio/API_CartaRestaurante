c992fa9fc34e09e9bdfa69f2b81a08b50f22a0d9
papacham97 <jveraozorio@gmail.com>
2023-04-25 15:26:44 -0300

    Inializacion del proyecto

---
+++ EjercicioIntegrador/src/main/java/com/TodocodeAPI/s/EjercicioIntegrador/EjercicioIntegradorApplication.java
@@ -0,0 +1,13 @@
+package com.TodocodeAPI.s.EjercicioIntegrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EjercicioIntegradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EjercicioIntegradorApplication.class, args);
+	}
+
+}
